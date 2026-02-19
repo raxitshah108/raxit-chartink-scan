b4d7ffad15a89e9c1c56ecf3ce1397f904ed33ae
raxitshah108 <121301898+raxitshah108@users.noreply.github.com>
2026-02-19 20:18:16 +0530

    Create raxit_bot.py

---
+++ raxit_bot.py
@@ -0,0 +1,34 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def get_chartink_data():
+    url = "https://chartink.com/screener/stocks-10-range-2"
+
+    with requests.Session() as s:
+        r = s.get(url)
+        soup = BeautifulSoup(r.text, "html.parser")
+
+        token = soup.find("meta", {"name": "csrf-token"})["content"]
+        s.headers["X-CSRF-TOKEN"] = token
+
+        payload = {
+            "scan_clause": ""
+        }
+
+        data = s.post("https://chartink.com/screener/process", data=payload).json()
+
+        stocks = []
+        for stock in data["data"]:
+            stocks.append(stock["nsecode"])
+
+        return stocks
+
+def send_telegram(message):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        "ch
+
